ffbffa8fd0e1fb895a9c3b581bba190aac377d5a
Shivam <thisisshivam06@gmail.com>
2022-05-25 17:08:26 +0530

    React with Html and css

--- src/index.js
+++ src/index.js
@@ -34,8 +34,8 @@
 
 ////////////////////// Javascript Expressions in JSX in React JS #10 /////////////////////
 
-import React from "react";
-import ReactDOM from "react-dom";
+// import React from "react";
+// import ReactDOM from "react-dom";
 
 // const flName = "shivam singh";
 
@@ -47,17 +47,58 @@ import ReactDOM from "react-dom";
 //   document.getElementById("root")
 // );
 
-const fName = "shivam";
-const lName = "singh";
+// const fName = "shivam";
+// const lName = "singh";
 
+// ReactDOM.render(
+//   <>
+//     <h1>{`my first name is ${fName} and my last name is ${lName}`}</h1>
+//     {/* <h1>my name is {fName + " " + lName}</h1> */}
+//     {/* <h1>
+//       my name is {fName} {lName}
+//     </h1> */}
+//     <p>my lucky number is {5 + 5}</p>
+//   </>,
+//   document.getElementById("root")
+// );
+
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+const name = "shivam";
+const img1 = "https://picsum.photos/200/300";
+const img2 = "https://picsum.photos/250/300";
+const img3 = "https://picsum.photos/300/300";
+const links = "https://www.thapatechnical.com";
+// .heading {
+//   color: #fa9191;
+//   text-align: center;
+//   text-transform: capitalize;
+//   font-weight: bold;
+//   text-shadow: 0px 2px 4px #ffe9c5;
+//   margin: 70px 0;
+//   font-family: "Josefin Sans", sans-serif;
+// }
+const heading = {
+  color: "#fa9191",
+  textTransform: "capitalize",
+  textAlign: "center",
+  fontWeight: "bold",
+  textShadow: "0px 2px 4px #ffe9c5",
+  margin: "70px 0",
+  fontFamily: '"Josefin Sans", sans-serif',
+};
 ReactDOM.render(
   <>
-    <h1>{`my first name is ${fName} and my last name is ${lName}`}</h1>
-    {/* <h1>my name is {fName + " " + lName}</h1> */}
-    {/* <h1>
-      my name is {fName} {lName}
-    </h1> */}
-    <p>my lucky number is {5 + 5}</p>
+    <h1 style={heading}>my name is {name}</h1>,
+    <div className="img_div">
+      <img src={img1} alt="randomImages" />
+      <img src={img2} alt="randomImages" />
+      <a href={links} target="_thapa">
+        <img src={img3} alt="randomImages" />
+      </a>
+    </div>
   </>,
   document.getElementById("root")
 );
